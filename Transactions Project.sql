ef506f6e509a13dc8f9f48948f2e23e68d852f0c
Ayanle32 <88679427+Ayanle32@users.noreply.github.com>
2024-01-18 01:07:21 +0000

    commit

--- Transactions Project.sql
+++ Transactions Project.sql
@@ -52,4 +52,23 @@ ORDER BY AVG(amount) DESC;
 
 -- Household Items have the highest average transaction amount and eating out has the least average transactions amount
 
+CREATE VIEW total_spends AS
+SELECT SUM(amount) AS "Total Spending", category 
+FROM transactions
+GROUP BY category
+ORDER BY SUM(amount) DESC;
+
+SELECT * FROM total_spends;
+
+-- So here we created a view for the total transactions for each category.
+-- This will be particularly useful for creating visualisations in some BI tool.
+
+CREATE VIEW monthly_spending AS
+SELECT SUM(amount) AS "Total Spending by Month", MONTH(transaction_date) AS "Month Number"
+FROM transactions
+GROUP BY MONTH(transaction_date)
+ORDER BY SUM(amount) DESC;
+
+SELECT * FROM monthly_spending;
+
 SELECT * FROM transactions;
